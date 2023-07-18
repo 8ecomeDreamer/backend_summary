d7269016fb8dbe4b95b1b3e035af2686e3cb10c5
8ecomeDreamer <1412415464@qq.com>
2023-07-18 17:18:25 +0800

    打卡更新

--- cases/01_了解java的基础语法/basic_grammer/src/main/java/variable/variable.java
+++ cases/01_了解java的基础语法/basic_grammer/src/main/java/variable/variable.java
@@ -7,5 +7,7 @@ public class variable {
         int age = 18;
         System.out.println("变量需要先声明后赋值 且不能重复声明");
         System.out.println("变量需要满足标志符规则；即不能用数字开头，不能是关键字，且内容必须为由数字，字母，下划线，美元符任意组成");
+
+
     }
 }
