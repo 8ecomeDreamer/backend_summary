37992fbc42fed2bbf5547ff621d0e6cb58e0d2fa
8ecomeDreamer <1412415464@qq.com>
2023-07-18 11:12:44 +0800

    打卡更新

---
+++ cases/01_了解java的基础语法/basic_grammer/src/main/java/iterator/iterator.java
@@ -0,0 +1,12 @@
+package iterator;
+
+public class iterator {
+    public static void main(String[] args) {
+//        java字面量分为 整数 小数 字符 字符串 布尔值
+        System.out.println(15);
+        System.out.println(15.0);
+        System.out.println('s'); // 字符串为单引号 而字符串为双引号
+        System.out.println("这是字符串");
+        System.out.println(true);
+    }
+}
---
+++ cases/01_了解java的基础语法/basic_grammer/src/main/java/variable/variable.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class variable {
+    public static void main(String[] args) {
+//        变量赋值格式 数据类型 变量名 = 数据
+//        变量具有作用域
+        int age = 18;
+        System.out.println("变量需要先声明后赋值 且不能重复声明");
+        System.out.println("变量需要满足标志符规则；即不能用数字开头，不能是关键字，且内容必须为由数字，字母，下划线，美元符任意组成");
+    }
+}
