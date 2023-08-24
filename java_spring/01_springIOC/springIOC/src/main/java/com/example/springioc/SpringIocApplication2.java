51d68c5646622a12f1c95b67078829ebba371746
8ecomeDreamer <1412415464@qq.com>
2023-08-24 17:07:38 +0800

    springioc的xml写法

--- java_spring/01_springIOC/springIOC/src/main/java/com/example/springioc/SpringIocApplication2.java
+++ java_spring/01_springIOC/springIOC/src/main/java/com/example/springioc/SpringIocApplication2.java
@@ -2,6 +2,7 @@
 package com.example.springioc;
 
 import com.example.springioc.dao.impl.BookDaoImpl;
+import com.example.springioc.service.impl.BookServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,6 +13,9 @@ public class SpringIocApplication2 {
 //    4.获取bean  参数为xml的id
         BookDaoImpl bookDao = (BookDaoImpl) ctx.getBean("BookDaoImpl");
         bookDao.save();
+
+        BookServiceImpl bookService =  (BookServiceImpl) ctx.getBean("BookServiceImpl");
+        bookService.save();
     }
 
 }
